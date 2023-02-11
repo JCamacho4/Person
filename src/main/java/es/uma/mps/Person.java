5b8371e047d2a16035e2237c45e87eeec45e7b14
JCamacho4 <jorgecamagarc@gmail.com>
2023-02-11 12:37:51 +0100

    Class constructor, and getters()

---
+++ src/main/java/es/uma/mps/Person.java
@@ -0,0 +1,26 @@
+package es.uma.mps;
+
+public class Person {
+
+    private final String name;
+    private final int age;
+    private final String gender; // Male, Female
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
