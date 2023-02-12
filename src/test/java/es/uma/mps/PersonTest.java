401d3e7ee2ac905563a47afb0f61f7f16fb74660
JCamacho4 <jorgecamagarc@gmail.com>
2023-02-12 12:42:54 +0100

    Added more tests on the average method

--- src/test/java/es/uma/mps/PersonTest.java
+++ src/test/java/es/uma/mps/PersonTest.java
@@ -55,11 +55,57 @@ class PersonTest {
         persons.add(new Person("Emily", 26, "Female"));
         persons.add(new Person("Sarah", 32, "Female"));
 
-        double[] result = person.averageAgePerGender(persons);
+        double[] result = Person.averageAgePerGender(persons);
 
         assertEquals(27.5, result[0], 0.1); // Male average age should be 27.5
         assertEquals(28.6, result[1], 0.1); // Female average age should be 29
     }
 
+    @Test
+    public void averageAgeOnEmptyList(){
+        double[] result = Person.averageAgePerGender(new ArrayList<Person>());
 
+        assertEquals(Double.NaN, result[0]);
+        assertEquals(Double.NaN, result[1]);
+    }
+
+    @Test
+    public void averageAgeOn3MalesAnd0Females(){
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("John", 42, "Male"));
+        persons.add(new Person("Michael", 80, "Male"));
+        persons.add(new Person("James", 16, "Male"));
+
+        double[] result = Person.averageAgePerGender(persons);
+
+        assertEquals(46, result[0], 0.1);
+        assertEquals(Double.NaN, result[1]);
+    }
+
+    @Test
+    public void averageAgeOn1MaleAnd1Female(){
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Lucas", 31, "Male"));
+        persons.add(new Person("Michelle", 57, "Female"));
+
+        double[] result = Person.averageAgePerGender(persons);
+
+        assertEquals(31, result[0]);
+        assertEquals(57, result[1]);
+    }
+
+    @Test
+    public void averageAgeOn1MaleAnd4Females(){
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Bob", 21, "Male"));
+        persons.add(new Person("Michael", 54, "Female"));
+        persons.add(new Person("Jane", 23, "Female"));
+        persons.add(new Person("Emily", 42, "Female"));
+        persons.add(new Person("Sarah", 55, "Female"));
+
+        double[] result = Person.averageAgePerGender(persons);
+
+        assertEquals(21, result[0]); // Male average age should be 27.5
+        assertEquals(43.5, result[1], 0.1); // Female average age should be 29
+    }
 }
