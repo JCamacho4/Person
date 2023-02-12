d872318955c9ce1356957b5951396faacd02bf72
JCamacho4 <jorgecamagarc@gmail.com>
2023-02-12 13:00:09 +0100

    Add documentation to tests

--- src/test/java/es/uma/mps/PersonTest.java
+++ src/test/java/es/uma/mps/PersonTest.java
@@ -8,32 +8,59 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class PersonTest contains tests for verifying the correctness of the Person class and its methods.
+ * The tests verify the creation of Person objects, the calculation of average age per gender and the exception handling.
+ *
+ * @author Jorge Camacho García
+ */
 class PersonTest {
     Person person;
 
     @AfterEach
     void shutdown(){ person = null;}
 
+    /**
+     * Test to verify the handling of an empty name in the Person class.
+     * Expects a BadArgumentsException to be thrown when a Person is created with an empty name.
+     */
     @Test
     void unnamedPerson(){
         assertThrows(BadArgumentsException.class, () -> person = new Person("",68,"Female"));
     }
 
+    /**
+     * Test to verify the handling of negative age in the Person class.
+     * Expects a BadArgumentsException to be thrown when a Person is created with negative age.
+     */
     @Test
     void negativeAgePerson(){
         assertThrows(BadArgumentsException.class, () -> person = new Person("Alice",-20, "Female"));
     }
 
+    /**
+     * Test to verify the handling of empty gender in the Person class.
+     * Expects a BadArgumentsException to be thrown when a Person is created with an empty gender.
+     */
     @Test
     void emptyGenderPerson(){
         assertThrows(BadArgumentsException.class, () -> person = new Person("Bob", 34, ""));
     }
 
+
+    /**
+     * Test to verify the handling of undefined gender in the Person class.
+     * Expects a BadArgumentsException to be thrown when a Person is created with an undefined gender.
+     */
     @Test
     void notIdentifiedGenderPerson(){
         assertThrows(BadArgumentsException.class, () -> person = new Person("Bob", 34, "Random"));
     }
 
+    /**
+     * Test to verify the correct creation of a Person object.
+     * Expects the created Person object to have the same name, age, and gender as the input parameters.
+     */
     @Test
     void correctPersonCreation(){
         String name = "Charles", gender = "Male";
@@ -46,6 +73,10 @@ class PersonTest {
         assertEquals(gender,person.getGender());
     }
 
+    /**
+     * Test to verify the calculation of average age per gender in a list of Person objects, with 2 males and 3 females.
+     * Expects the Male average to be 27.5 and the Female average to be 28.6.
+     */
     @Test
     public void averageAgeOn2MalesAnd3Females() {
         List<Person> persons = new ArrayList<>();
@@ -61,6 +92,10 @@ class PersonTest {
         assertEquals(28.6, result[1], 0.1); // Female average age should be 29
     }
 
+    /**
+     * Test to verify the calculation of average age per gender in an empty list of Person objects.
+     * Expects the result to be NaN for both Male and Female averages.
+     */
     @Test
     public void averageAgeOnEmptyList(){
         double[] result = Person.averageAgePerGender(new ArrayList<Person>());
@@ -69,6 +104,11 @@ class PersonTest {
         assertEquals(Double.NaN, result[1]);
     }
 
+    /**
+     * Test to verify the calculation of average age per gender in a
+     * list of Person objects containing 3 males and 0 females.
+     * Expects the Male average to be 46 and the Female average to be NaN.
+     */
     @Test
     public void averageAgeOn3MalesAnd0Females(){
         List<Person> persons = new ArrayList<>();
@@ -82,6 +122,11 @@ class PersonTest {
         assertEquals(Double.NaN, result[1]);
     }
 
+    /**
+     * Test to verify the calculation of average age per gender in a list
+     * of Person objects containing 1 Male and 1 Female.
+     * Expects the Male average to be 31 and the Female average to be 57.
+     */
     @Test
     public void averageAgeOn1MaleAnd1Female(){
         List<Person> persons = new ArrayList<>();
@@ -94,6 +139,10 @@ class PersonTest {
         assertEquals(57, result[1]);
     }
 
+    /**
+     * Test to verify the calculation of average age per gender in a list of Person objects containing 1 male and 4 females.
+     * Expects the Male average to be 21 and the Female average to be 43.5.
+     */
     @Test
     public void averageAgeOn1MaleAnd4Females(){
         List<Person> persons = new ArrayList<>();
